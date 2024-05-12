22fcc31524ae51c0225b8b5a0deb07456730f3d7
vinayg-me <work@vinayg.me>
2024-05-12 11:00:34 +0530

    Added cloudinary url, instead of local

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container h-svh mx-auto flex justify-center align-middle flex-col items-center">
       <TestimonialCard
-        avatarUrl="/src/assets/img/profile-thumbnail.png"
+        avatarUrl="https://res.cloudinary.com/djendzbtu/image/upload/v1715491693/profile-thumbnail_hfwh2o.png"
         title="Sarah Dole"
         subTitle="@sarahdole"
       >
--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 
 type TestimonialCardType = {
     avatarUrl: string;
@@ -9,32 +9,13 @@ type TestimonialCardType = {
 
 const TestimonialCard: React.FC<TestimonialCardType> = (props) => {
     const { children, avatarUrl, title, subTitle } = props;
-    const [imgLogo, setImgLogo] = useState<string | undefined>('');
 
-  useEffect(() => {
-    const loadImage = async () => {
-        try {
-          const response = await fetch(`${avatarUrl}`);
-          if (response.ok) {
-            const blob = await response.blob();
-            const imageUrl = URL.createObjectURL(blob);
-            setImgLogo(imageUrl);
-          } else {
-            console.error("Failed to fetch image:", response.statusText);
-          }
-        } catch (error) {
-          console.error("Error loading image:", error);
-        }
-      };
-
-    loadImage();
-  }, [avatarUrl]);
     return (
         <div className="flex bg-white sm:w-80 md:w-96 rounded-lg min-h-60 justify-self-center p-4">
             <div className="grid grid-rows-3 w-full">
                 <div className="grid row-span-1 grid-cols-4">
                     <div className="col-span-1 flex justify-center items-center">
-                        <img src={imgLogo} alt="Testimonial Profile Picture" className="w-12 h-12 rounded-full" loading="lazy" />
+                        <img src={avatarUrl} alt="Testimonial Profile Picture" className="w-12 h-12 rounded-full" loading="lazy" />
                     </div>
                     <div className="col-span-3 flex flex-col justify-center break-words">
                         <p className="font-semibold text-left text-lg">{title}</p>
